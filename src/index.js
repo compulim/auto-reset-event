4563c9d261835b908bce4664e1033ca532095216
William Wong <compulim@hotmail.com>
2018-10-03 13:24:24 -0700

    Remove direct dependency to @babel/runtime

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-// As a de facto standard, NPM packages should be targeting ES5
-import '@babel/runtime';
-
 import createAutoResetEvent from './createAutoResetEvent';
 
 export default createAutoResetEvent
