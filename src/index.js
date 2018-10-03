8e4712516b941d648ca245cc4a0b1171056d6d06
William Wong <compulim@hotmail.com>
2018-10-03 13:18:42 -0700

    Move @babel/runtime to dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // As a de facto standard, NPM packages should be targeting ES5
-// import '@babel/runtime';
+import '@babel/runtime';
 
 import createAutoResetEvent from './createAutoResetEvent';
 
